5c05f636a87eef8ce281f7e23f1f468f86b9083c
CMcCown <connermccown94@gmail.com>
2022-04-12 12:43:15 -0600

    created async password hashing function

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -45,6 +46,14 @@ User.init(
       }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+          },
+
       sequelize,
       timestamps: false,
       freezeTableName: true,
